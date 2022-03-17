d450c71040557a3135c56b9bc17682ba79650c20
jimmyrobert13 <35510447+jimmyrobert13@users.noreply.github.com>
2022-03-17 17:20:11 -0300

    Construnção Dashboard responsivo

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '36378',
+        paymentStatus: 'due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '49347',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+]
